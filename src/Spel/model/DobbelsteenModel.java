cf975948f53daae04d924c3c90448a8221ebc867
yassine <teenbarba@gmail.com>
2017-03-15 23:03:43 +0100

    HGFTHFYTFH

--- src/Spel/model/DobbelsteenModel.java
+++ src/Spel/model/DobbelsteenModel.java
@@ -31,8 +31,9 @@ public class DobbelsteenModel {
 
     public void werp(boolean reset) {
         int[][] worpen = new int[20][5];
-        worpen[0] = new int[]{1,6,2,6,6};
+        worpen[0] = new int[]{1,3,2,6,6};
         worpen[1] = new int[]{2,4,2,4,4};
+        worpen[2] = new int[]{5,4,3,2,1};
         worpen[2] = new int[]{5,5,5,5,5};
         worpen[3] = new int[]{3,3,3,6,6};
         worpen[4] = new int[]{4,6,2,6,6};
